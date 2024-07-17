d99d9f31a9b10282edfdb69b90b8418213e7a4a4
khomsandev <bas.rmu60@gmail.com>
2024-07-17 13:22:30 +0700

    show courses

--- backend/src/courses.controller.ts
+++ backend/src/courses.controller.ts
@@ -5,8 +5,22 @@ export class CoursesController {
 
   @Get()
   findAll(): any {
-    return {
-        message:'Good morning!'
-    };
+    return [
+      {
+        id: '100',
+        number: '01204111',
+        title: 'Compoter and Programing'
+      },
+      {
+        id: '324bvf',
+        number: '01204132',
+        title: 'Discreate mathematics and linear algebra'
+      },
+      {
+        id: '552kjh',
+        number: '01204156',
+        title: 'Design and analysis of algorithms'
+      }
+    ];
   }
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,48 +2,28 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [message, setMessage] = useState('My message 123');
+  const [courses, setCourses] = useState<any[]>([]);
 
   useEffect(() => {
     fetch('http://localhost:3000/courses')
         .then(res => res.json())
-        .then(obj => {
-          setMessage(obj.message);
+        .then(courses => {
+          console.log(courses);
+          setCourses(courses);
         });
   },[]);
 
   return (
     <div className='App'>
-      {message}
+      <ul>
+        {courses.map((item) => (
+          <li key={item.id}>
+            {item.number} - {item.title}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
-
-/*
-type AppState = {
-  message: string;
-};
-
-class App extends React.Component<{}, AppState> {
-  state: AppState = {
-    message: 'Default message!',
-  };
-
-  componentDidMount() {
-      fetch('http://localhost:3000/courses')
-        .then(res => res.json())
-        .then(obj => {
-          this.setState({message: obj.message});
-        });
-  }
-
-  render() {
-    return (
-      <div>
-        {this.state.message}
-      </div>
-    );
-  }
-}*/
 
 export default App;
